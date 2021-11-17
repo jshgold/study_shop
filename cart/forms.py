e73ab0229b622631c1d53b9b27677d53caf5cbff
jshgold <jshgold@naver.com>
2021-11-17 19:37:14 +0900

    cart_views

---
+++ cart/forms.py
@@ -0,0 +1,6 @@
+from .models import *
+from django.forms import Form
+
+
+class AddProductForm(Form.ModelForm):
+    pass
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+
+from item.models import *
+from .forms import AddProductForm
+from .cart import Cart
+
+def add(request,product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product,id=product_id)
+
+    form = AddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'],
+        is_update=cd['is_update'])
+
+    return redirect('cart:detail')
+    
+
+
+
+
+
