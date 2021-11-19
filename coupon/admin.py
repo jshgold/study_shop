c6b7bb4c9f466c4f02ed7a09b0de5dbed3ca53e2
jshgold <jshgold@naver.com>
2021-11-19 19:24:00 +0900

    coupon admin

--- coupon/admin.py
+++ coupon/admin.py
@@ -4,7 +4,8 @@ from .models import Coupon
 
 
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ['code','use_from','use_to']
+    list_display = ['code','use_from','use_to','amount','active']
+    list_editable = ['use_from','use_to','amount','active']
 
 
 admin.site.register(Coupon,CouponAdmin)
